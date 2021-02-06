e79df74b8ae1d4b65a92090ed7486dc07388ba2c
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-06 20:44:06 +0300

    feat: functions code add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    let min = Math.min(...array);
+    if (min == Infinity) {
+        min = 0;
+    } else if (isNaN(min)) {
+        min = 0;
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = Math.max(...array);
+    if (max == Infinity) {
+        max = 0;
+    } else if (isNaN(max)) {
+        max = 0;
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    function avg(array) {
+        let arraySum = array.reduce((sum, item) => sum + item, 0);
+        let avg = arraySum / array.length;
+        if (isNaN(avg)) {
+            avg = 0;
+        }
+        return avg;
+    }
+};
